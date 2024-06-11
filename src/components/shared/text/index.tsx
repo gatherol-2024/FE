58819811e91876de45011697916ffb64fd273907
dohi0512 <donghuni512@gmail.com>
2024-06-11 23:00:46 +0900

    Feat:: 텍스트 컴포넌트

---
+++ src/components/shared/text/index.tsx
@@ -0,0 +1,24 @@
+import { HTMLAttributes } from "react";
+import font from "../../../design-token/font";
+import { CSSProperties } from "styled-components";
+
+import styled from "styled-components";
+type Font = keyof typeof font;
+interface TextProps extends HTMLAttributes<HTMLSpanElement> {
+  type?: Font;
+  whiteSpace?: CSSProperties["whiteSpace"];
+  color?: CSSProperties["color"];
+}
+const Text = ({ color, children, whiteSpace = "nowrap", type = "base" }: TextProps) => {
+  return (
+    <StyledText style={{ color, whiteSpace }} type={type}>
+      {children}
+    </StyledText>
+  );
+};
+
+const StyledText = styled.span<{ type: Font }>`
+  ${({ type }) => font[type]}
+`;
+
+export default Text;
