ed536a250b229b886b77774dca18ad06227584ed
dohi0512 <donghuni512@gmail.com>
2024-06-12 16:45:30 +0900

    Refactoring:: 안쓰는 타입 삭제

--- src/components/shared/button/type.ts
+++ src/components/shared/button/type.ts
@@ -1,3 +1,3 @@
 export type ButtonSize = "xs" | "sm" | "md" | "lg" | "xl" | "full";
 export type ButtonState = "default" | "active" | "disabled";
-export type ButtonType = "primary" | "ghost" | "text" | "outline";
+export type ButtonType = "primary" | "ghost" | "text";
