944c1ea0fa5fd0fbfe4b7a75bfb3901d94c78209
dohi0512 <donghuni512@gmail.com>
2024-06-12 16:45:54 +0900

    Style:: Text 컴포넌트 기본값 수정

--- src/components/shared/text/index.tsx
+++ src/components/shared/text/index.tsx
@@ -1,7 +1,6 @@
 import { HTMLAttributes } from "react";
 import font from "../../../design-token/font";
 import { CSSProperties } from "styled-components";
-
 import styled from "styled-components";
 type Font = keyof typeof font;
 interface TextProps extends HTMLAttributes<HTMLSpanElement> {
@@ -9,7 +8,7 @@ interface TextProps extends HTMLAttributes<HTMLSpanElement> {
   whiteSpace?: CSSProperties["whiteSpace"];
   color?: CSSProperties["color"];
 }
-const Text = ({ color, children, whiteSpace = "nowrap", type = "base" }: TextProps) => {
+const Text = ({ color = "black", children, whiteSpace = "wrap", type = "base" }: TextProps) => {
   return (
     <StyledText style={{ color, whiteSpace }} type={type}>
       {children}
