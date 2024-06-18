09ac3b09f40716df32c2e2b3b4467de900150cff
dohi0512 <donghuni512@gmail.com>
2024-06-18 21:59:17 +0900

    Style:: 공통 레이아웃

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,26 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/shared/header";
 import "./App.css";
 import Main from "./pages/main";
+import BoardDetail from "./components/board";
+import VoicePage from "./pages/voice";
+import styled from "styled-components";
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<Main />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/board" element={<BoardDetail />} />
+          <Route path="/voice" element={<VoicePage />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
 
+const Layout = styled.div`
+  flex: 1;
+  display: flex;
+`;
 export default App;
