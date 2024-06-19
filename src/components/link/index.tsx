07745531bff1c3fcd49dad9666d715196edb36cf
dohi0512 <donghuni512@gmail.com>
2024-06-19 16:56:20 +0900

    Feat:: Link  컴포넌트

---
+++ src/components/link/index.tsx
@@ -0,0 +1,22 @@
+import { AnchorHTMLAttributes } from "react";
+import styled, { CSSProperties } from "styled-components";
+import font from "../../design-token/font";
+import Text from "../shared/text";
+type Font = keyof typeof font;
+interface LinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
+  type: Font;
+  color?: CSSProperties["color"];
+}
+const LinkText = ({ children, ...props }: LinkProps) => {
+  return (
+    <StyledLink {...props}>
+      <Text {...props}>{children}</Text>
+    </StyledLink>
+  );
+};
+
+const StyledLink = styled.a`
+  text-decoration: none;
+`;
+
+export default LinkText;
