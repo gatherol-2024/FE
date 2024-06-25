a5ac465283f738079b95a893db6851efdad1ebd7
dohi0512 <donghuni512@gmail.com>
2024-06-25 22:13:53 +0900

    Style:: 다크모드 색상 분리 및 공통 색 지정

--- src/design-token/color.ts
+++ src/design-token/color.ts
@@ -1,8 +1,17 @@
+import { css } from "styled-components";
+
 const commonColor = {
   white: "#ece8e1",
   danger: "#ff4c4c",
   primary: "#f85149",
   success: "#20c997",
+  gray100: "#edf0f3",
+  gray200: "#c5c8cb",
+  gray300: "#9da0a3",
+  gray400: "#75787b",
+  gray500: "#4d5053",
+  gray600: "#25282b",
+  black: "#000",
 };
 
 export const dark = {
@@ -11,7 +20,7 @@ export const dark = {
   ...commonColor,
 };
 export const light = {
-  background: "#ece8e1",
+  background: "#fff",
   text: "#1c1c1c",
   ...commonColor,
 };
