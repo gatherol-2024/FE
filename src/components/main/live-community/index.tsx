7e4d37891d2b5c21108ca9460a267d3ca9867c63
dohi0512 <donghuni512@gmail.com>
2024-06-26 21:08:18 +0900

    Feat:: 라이브게시판 카테고리 추가

--- src/components/main/live-community/index.tsx
+++ src/components/main/live-community/index.tsx
@@ -1,11 +1,19 @@
 import styled from "styled-components";
 import Text from "../../shared/text";
 import BoardItem from "./borad-item";
+import Dropdown from "../../shared/dropdown";
 
 const LiveCommunity = () => {
   return (
     <StyledCommunity>
       <Text type="3xl">라이브 커뮤니티</Text>
+      <CommunityMenu>
+        <Dropdown title="전체">
+          <Dropdown.Item selected>전체</Dropdown.Item>
+          <Dropdown.Item>팀 구하기</Dropdown.Item>
+          <Dropdown.Item>잡담</Dropdown.Item>
+        </Dropdown>
+      </CommunityMenu>
       <LiveBoard>
         <BoardItem isLive={true} title="게임 같이 하실분 구해요" author="PodoLove#LOVE" />
         <BoardItem isLive={false} title="게임 같이 하실분 구해요" author="PodoLove#LOVE" />
@@ -26,6 +34,11 @@ const StyledCommunity = styled.div`
   flex-direction: column;
   gap: 1.5rem;
   align-items: center;
+`;
+const CommunityMenu = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
 `;
 
 const LiveBoard = styled.div`
