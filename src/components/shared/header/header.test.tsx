c3e6ac7f7f54e5a829fe998ba0240ca631b3a913
dohi0512 <donghuni512@gmail.com>
2024-07-09 20:35:40 +0900

    Test:: recoil init value 설정

--- src/components/shared/header/header.test.tsx
+++ src/components/shared/header/header.test.tsx
@@ -1,11 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import Header from ".";
 import { RecoilRoot } from "recoil";
+import { theme, useThemeStore } from "../../../store/theme";
 
 describe("헤더 테스트", () => {
   it("로그인 돼있음", () => {
     render(
-      <RecoilRoot>
+      <RecoilRoot initializeState={(snapshot) => snapshot.set(theme, "dark")}>
         <Header />
       </RecoilRoot>
     );
