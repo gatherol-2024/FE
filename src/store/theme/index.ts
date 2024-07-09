8d2d4ea1a1d036ed78b24bc685159e037431fbce
dohi0512 <donghuni512@gmail.com>
2024-07-09 20:35:56 +0900

    Chore:: export 추가

--- src/store/theme/index.ts
+++ src/store/theme/index.ts
@@ -3,7 +3,7 @@ import { recoilPersist } from "recoil-persist";
 
 const { persistAtom } = recoilPersist();
 
-const theme = atom({
+export const theme = atom({
   key: "theme",
   default: "light",
   effects_UNSTABLE: [persistAtom],
