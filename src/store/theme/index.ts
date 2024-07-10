b1ead19f1f186b58414bec54455f89bed2cdf439
dohi0512 <donghuni512@gmail.com>
2024-07-10 22:27:54 +0900

    Chore:: 변수명 수정

--- src/store/theme/index.ts
+++ src/store/theme/index.ts
@@ -3,12 +3,12 @@ import { recoilPersist } from "recoil-persist";
 
 const { persistAtom } = recoilPersist();
 
-export const theme = atom({
+export const themeState = atom({
   key: "theme",
   default: "light",
   effects_UNSTABLE: [persistAtom],
 });
 
-export const useThemeStore = () => useRecoilState(theme);
-export const useThemeValueStore = () => useRecoilValue(theme);
-export const useSetThemeStore = () => useSetRecoilState(theme);
+export const useThemeStore = () => useRecoilState(themeState);
+export const useThemeValueStore = () => useRecoilValue(themeState);
+export const useSetThemeStore = () => useSetRecoilState(themeState);
