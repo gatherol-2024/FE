8ad4b8b5f15fa2340284b4cc96832b211d3a6aca
dohi0512 <donghuni512@gmail.com>
2024-07-23 23:08:15 +0900

    Test:: 댓글 개수 테스트

---
+++ src/components/board/comments/comments.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import { RecoilRoot } from "recoil";
+import Comments from ".";
+const MOCK_COMMENTS = [{ author: "AUTHOR_TEST", comment: "COMMENT_TEST", date: "DATE_TEST" }];
+describe("댓글 테스트", () => {
+  describe("1. 댓글 개수", () => {
+    it("1.1 댓글 없을 때", () => {
+      render(
+        <RecoilRoot>
+          <Comments comments={[]} addComment={() => {}} />
+        </RecoilRoot>
+      );
+      const element = screen.getByTestId("comment-cnt");
+      expect(element).toHaveTextContent("0");
+    });
+    it("1.1 댓글 없을 때", () => {
+      render(
+        <RecoilRoot>
+          <Comments comments={MOCK_COMMENTS} addComment={() => {}} />
+        </RecoilRoot>
+      );
+      const element = screen.getByTestId("comment-cnt");
+      expect(element).toHaveTextContent("1");
+    });
+  });
+});
+
+export {};
