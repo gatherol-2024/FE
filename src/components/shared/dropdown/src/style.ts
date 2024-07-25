ce06475d64be7918ae04dad7039262fa8bc4a831
dohi0512 <donghuni512@gmail.com>
2024-07-25 22:39:59 +0900

    Style:: 폰트 사이즈 수정

--- src/components/shared/dropdown/src/style.ts
+++ src/components/shared/dropdown/src/style.ts
@@ -1,10 +1,10 @@
 import sizeGenerator from "../../../../utils/sizeGenerator";
 
 export const getDropdownSize = {
-  xs: sizeGenerator(1.5, 0.25, 3, "sm"),
-  sm: sizeGenerator(2, 0.5, 4, "base"),
-  md: sizeGenerator(2.5, 0.75, 5, "lg"),
-  lg: sizeGenerator(3, 1, 6, "xl"),
-  xl: sizeGenerator(3.25, 1.25, 7, "2xl"),
+  xs: sizeGenerator(1.5, 0.5, 3, "xs"),
+  sm: sizeGenerator(2, 0.75, 4, "sm"),
+  md: sizeGenerator(2.5, 1, 5, "base"),
+  lg: sizeGenerator(3, 1.25, 6, "lg"),
+  xl: sizeGenerator(3.25, 1.5, 7, "xl"),
   full: sizeGenerator(3.5, 0.5, 8, "base"),
 };
