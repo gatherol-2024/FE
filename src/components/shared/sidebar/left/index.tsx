1e5544b41efdce44151fa0a0670909f856986109
dohi0512 <donghuni512@gmail.com>
2024-08-30 21:57:41 +0900

    Add:: 왼쪽바 링크 설정

--- src/components/shared/sidebar/left/index.tsx
+++ src/components/shared/sidebar/left/index.tsx
@@ -2,20 +2,26 @@ import styled from "styled-components";
 import LeftItem from "./item";
 import { FaUser } from "react-icons/fa";
 import { IoMdChatboxes } from "react-icons/io";
+import { MdVoiceChat } from "react-icons/md";
 const LeftSideBar = () => {
   return (
     <StyledSidebar>
-      <LeftItem text="내 프로필" active={true}>
+      <LeftItem text="라이브 커뮤니티" link="/">
+        <IoMdChatboxes />
+      </LeftItem>
+      <LeftItem text="내 프로필" link="/profile">
         <FaUser />
       </LeftItem>
-      <LeftItem text="라이브 커뮤니티">
-        <IoMdChatboxes />
+
+      <LeftItem text="팀보이스" link="/voice">
+        <MdVoiceChat />
       </LeftItem>
     </StyledSidebar>
   );
 };
 
 const StyledSidebar = styled.div`
+  flex-shrink: 0;
   width: 12rem;
   display: flex;
   gap: 0.25rem;
