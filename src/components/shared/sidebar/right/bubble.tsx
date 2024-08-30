4a05c8183bc0cbc97db0a376e6f6361abef1dbec
dohi0512 <donghuni512@gmail.com>
2024-08-30 21:56:36 +0900

    Add:: 오른쪽 바 디바이스 관리창

---
+++ src/components/shared/sidebar/right/bubble.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+const Bubble = ({ text }: { text: string }) => {
+  return (
+    <StyledBubble>
+      <span>{text}</span>
+      <div></div>
+    </StyledBubble>
+  );
+};
+
+const StyledBubble = styled.div`
+  padding: 0.5rem;
+  border-radius: 0.25rem;
+  background-color: ${({ theme }) => theme.background};
+  color: ${({ theme }) => theme.text};
+  font-weight: bold;
+  opacity: 0;
+  transition: 0.2s;
+  white-space: nowrap;
+  bottom: 125%;
+  position: absolute;
+
+  div {
+    top: 100%;
+    left: 50%;
+    position: absolute;
+    width: 0.5rem;
+    height: 0.5rem;
+    background-color: inherit;
+    transform: translate(-50%, -50%) rotate(45deg);
+  }
+`;
+export default Bubble;
