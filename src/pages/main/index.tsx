5a679c0a525acc63d1440ec33f53782bfde4d396
dohi0512 <donghuni512@gmail.com>
2024-08-30 21:57:53 +0900

    Style:: 디자인 변경

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -15,12 +15,4 @@ const StyledMain = styled.div`
   width: 100%;
 `;
 
-const MainText = styled.div`
-  z-index: 10;
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  gap: 2rem;
-`;
-
 export default Main;
