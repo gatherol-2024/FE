e1871410a074c295a68ce43406fe8b7bd05af389
dohi0512 <donghuni512@gmail.com>
2024-09-04 20:22:27 +0900

    Add:: 게임정보 받아와서 프로필 설정

--- src/pages/voice/index.tsx
+++ src/pages/voice/index.tsx
@@ -3,18 +3,23 @@ import Profile from "../../components/voice/profile";
 import Button from "../../components/shared/button";
 import { IoMic, IoHeadset, IoCall } from "react-icons/io5";
 import useVoiceChat from "../../hooks/useVoiceChat";
+import { DATA } from "../../constants/test";
+import useInGame from "../../hooks/useInGame";
 
 const VoicePage = () => {
   const { teamvoice, connect, disconnect } = useVoiceChat();
-  console.log("teamvoice", teamvoice);
+  const { champSelect, gameSession } = useInGame();
+  // console.log(champSelect, gameSession);
+  const Profiles = DATA.data.myTeam.map((props: any, idx: number) => <Profile key={idx} {...props} />);
   return (
     <StyledVoice>
       <ProfilLayout>
-        <Profile position="TOP" name="PodoLove#LOVE" volume={100} />
+        {Profiles}
+        {/* <Profile position="TOP" data={} name="PodoLove#LOVE" volume={100} />
         <Profile position="JGL" name="고급진 솔로킬#KR1" />
         <Profile position="MID" name="강서구청장#i1kCC" mic={false} />
         <Profile position="BOT" name="앙태현짱짱맨#KR1" headset={false} />
-        <Profile position="SUP" name="Vitenci#KR1" mic={false} headset={false} />
+        <Profile position="SUP" name="Vitenci#KR1" mic={false} headset={false} /> */}
       </ProfilLayout>
       <VoiceMenu>
         <Button size="icon" type="icon" color="gray300" onClick={connect}>
