d12d5618b4200aa9f3c2f24fe6ccc72a22d8ad1d
dohi0512 <donghuni512@gmail.com>
2024-09-05 22:23:23 +0900

    Update:: 글 자세히보기 사이드바 삭제

--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import Content from "../../components/board/content";
-import Sidebar from "../../components/board/sidebar";
 import Comments from "../../components/board/comments";
 import { useParams } from "react-router-dom";
 import { useBoard } from "../../hooks/useBoard";
@@ -8,10 +7,8 @@ import { useBoard } from "../../hooks/useBoard";
 const BoardDetail = () => {
   const { id } = useParams();
   const { boardData, commentMutate } = useBoard(id);
-  console.log("boardData", boardData);
   return (
     <StyledBoard>
-      <Sidebar />
       <ContentLayout>
         <Content {...boardData} />
         <Comments addComment={commentMutate} comments={boardData?.comments} />
@@ -21,15 +18,11 @@ const BoardDetail = () => {
 };
 
 const StyledBoard = styled.div`
-  width: 100%;
-  position: relative;
-  display: grid;
-  grid-template-columns: 1fr 3fr 1fr;
-  gap: 1rem;
-  padding: 0 4rem 6rem;
+  height: 100%;
 `;
 
 const ContentLayout = styled.div`
+  height: 100%;
   display: flex;
   flex-direction: column;
   gap: 1.5rem;
