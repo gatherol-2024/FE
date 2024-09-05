c80eb2de577aeb3106d2559e47dc212e0616c42a
dohi0512 <donghuni512@gmail.com>
2024-09-05 22:22:27 +0900

    Refactoring:: 팀보이스 프로필 데이터 매칭

--- src/pages/voice/index.tsx
+++ src/pages/voice/index.tsx
@@ -9,18 +9,10 @@ import useInGame from "../../hooks/useInGame";
 const VoicePage = () => {
   const { teamvoice, connect, disconnect } = useVoiceChat();
   const { champSelect, gameSession } = useInGame();
-  // console.log(champSelect, gameSession);
   const Profiles = DATA.data.myTeam.map((props: any, idx: number) => <Profile key={idx} {...props} />);
   return (
     <StyledVoice>
-      <ProfilLayout>
-        {Profiles}
-        {/* <Profile position="TOP" data={} name="PodoLove#LOVE" volume={100} />
-        <Profile position="JGL" name="고급진 솔로킬#KR1" />
-        <Profile position="MID" name="강서구청장#i1kCC" mic={false} />
-        <Profile position="BOT" name="앙태현짱짱맨#KR1" headset={false} />
-        <Profile position="SUP" name="Vitenci#KR1" mic={false} headset={false} /> */}
-      </ProfilLayout>
+      <ProfilLayout>{Profiles}</ProfilLayout>
       <VoiceMenu>
         <Button size="icon" type="icon" color="gray300" onClick={connect}>
           <IoHeadset />
