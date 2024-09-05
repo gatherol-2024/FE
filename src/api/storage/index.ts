09745f52c21389785b3549b627c0673474a42fd6
dohi0512 <donghuni512@gmail.com>
2024-09-05 22:21:11 +0900

    Delete

--- src/api/storage/index.ts
+++
@@ -1,9 +0,0 @@
-export class Storage {
-  static getItem(key: string) {
-    return typeof window !== "undefined" ? localStorage.getItem(key) : null;
-  }
-  static setItem(key: string, value: string) {
-    if (typeof window === "undefined") return;
-    localStorage.setItem(key, value);
-  }
-}
